edd6732a5c3c431c978230b2d67e9f76799bb456
Simon Beal <sb48@sanger.ac.uk>
2015-07-07 15:54:34 +0100

    Add analysis of 'hidden' nodes

--- moniter.py
+++ moniter.py
@@ -1,4 +1,4 @@
-import subprocess, json, tempfile
+import subprocess, json, tempfile, os
 
 docker = "docker"
 moniter = "WwDocker-"
@@ -13,21 +13,46 @@ class Moniter(object):
         #Parse it
         node_data = self.parse_cluster(cluster_dict)
         #Continue parsing hidden nodes
-        #self.parse_hidden_nodes(node_data["hidden"])
+        self.parse_hidden_nodes(node_data["hidden"])
         #Output
         print json.dumps(node_data, indent = 4)
 
     def parse_hidden_nodes(self, hidden_nodes):
-        tmpfile = tempfile.NamedTemporaryFile(prefix = "docker_moniter_")
-        tmpfile.write("\n".join(hidden_nodes.keys()))
-        cmd = "dsh -f %s -M 'grep -Fc \"UPLOADED FILE AFTER \" /cgp/datastore/oozie-*/generated-scripts/*vcfUpload_*.stdout'"%tmpfile.name
+        #Parse the nodes that aren't running either the docker or the monitering system
+        #A temp file is needed for the dsh command with the names of every node it is to run the command on
+        tmp_name = self.make_tmp_file("\n".join(hidden_nodes.keys()))
+        cmd = "dsh -f %s -M 'grep -Fc \"UPLOADED FILE AFTER \" /cgp/datastore/oozie-*/generated-scripts/*vcfUpload_*.stdout'"%tmp_name
+        #Run the command
         sub = subprocess.Popen(cmd, shell=True,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
         stdout_buf, stderr_buf = sub.communicate()
-        print cmd
-        print stdout_buf
-        print stderr_buf
+        os.remove(tmp_name)
+        #If the string 'UPLOADED FILE AFTER ' wasn't found, update the list
+        for node in self.get_nodes(stderr_buf):
+            hidden_nodes[node] = "Further analysis necessary"
+        #For every other node, get the log data
+        nodes = self.get_nodes(stdout_buf)
+        tmp_name = self.make_tmp_file("\n".join(nodes))
+        cmd = "dsh -f %s -M 'ls /cgp/datastore/*.ini'"%tmp_name
+        sub = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        stdout_buf, _ = sub.communicate()
+        for node_info in stdout_buf.rstrip().split("\n"):
+            node, info = node_info.split(":")
+            hidden_nodes[node] = info[1:]
+        os.remove(tmp_name)
+    
+    def get_nodes(self, out):
+        #Returns the names of nodes in a list of nodes
+        return map(lambda node: node.split(":")[0], out.rstrip().split("\n"))
+
+    def make_tmp_file(self, contents):
+        #Create a temp file and write to it the contents given and return its name
+        tmpfile = tempfile.NamedTemporaryFile(prefix = "docker_moniter_", delete=False)
+        tmpfile.write(contents)
+        tmp_name = tmpfile.name
+        tmpfile.close()
+        return tmp_name
 
     def parse_cluster(self, cluster):
         node_data = {"idle hosts": {},

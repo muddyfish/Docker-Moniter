738ffd9dc4ed4d2d5508ce61c9bbf92926b3b2bd
Simon Beal <sb48@sanger.ac.uk>
2015-07-09 13:36:57 +0100

    Update python check analysis so it is identical in funtion to perl version

--- pancancer_check_analysis.py
+++ pancancer_check_analysis.py
@@ -8,8 +8,8 @@ DEFAULT_PATH = "http://pancancer.info/gnos_metadata/latest/"
 
 class AnalysisCheck(object):
     def __init__(self):
-        self.load_donors()
-        print self.find_json_gz()
+        self.donors = self.load_donors()
+        self.find_json_gz()
     
     def find_json_gz(self):
         directory = urllib2.urlopen(DEFAULT_PATH)
@@ -17,12 +17,23 @@ class AnalysisCheck(object):
         directory.close()
         gz_file = StringIO.StringIO(urllib2.urlopen(urlparse.urljoin(DEFAULT_PATH, filename)).read())
         json_data = map(json.loads, gzip.GzipFile(fileobj=gz_file).readlines())
-        for i in json_data:
-            print i["donor_unique_id"]
-            print
-        #print json_data
+        done = []
+        pending = []
+        for donor in json_data:
+            if donor["donor_unique_id"] in self.donors:
+                if "sanger" in donor["flags"]["variant_calling_performed"]:
+                    done.append(donor["donor_unique_id"])
+                else:
+                    pending.append(donor["donor_unique_id"])
+        print "Done:\n", "\n".join(sorted(done))
+        print "\n\n"
+        print "Pending:\n", "\n".join(sorted(map(lambda x: x.replace("::", "\t"), pending)))
 
-    def load_donors(self): pass
+    def load_donors(self):
+        donor_file = open("donors.lst")
+        donors = map(lambda line: line[:-1].replace("\t", "::"), donor_file.readlines())
+        donor_file.close()
+        return donors
 
 def main():
     AnalysisCheck()

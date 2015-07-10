028b272c384548a4de72e30abacc53573cd46958
Simon Beal <sb48@sanger.ac.uk>
2015-07-10 15:15:45 +0100

    Add ProxyAdaptor Sends user agent to the proxy Works now :)

--- pancancer_check_analysis.py
+++ pancancer_check_analysis.py
@@ -1,9 +1,15 @@
+#Python-2.7.9
 import requests, re, gzip, json, os
 import posixpath
+import ssl
+from requests.adapters import HTTPAdapter
 try:
     import cStringIO as StringIO
 except ImportError:
-    import StringIO
+    try:
+        import StringIO
+    except ImportError:
+        import io as StringIO
 
 workflow_id = "sanger_workflow"
 
@@ -11,22 +17,29 @@ DEFAULT_PATH = "http://pancancer.info/gnos_metadata/latest/"
 GITHUB_URL = "http://github.com/"
 REPO_URL = "ICGC-TCGA-PanCancer/pcawg-operations/tree/develop/variant_calling/%s/whitelists/sanger"%workflow_id
 SEARCH_FILES = 'ICGC-TCGA-PanCancer/pcawg-operations/blob/develop/variant_calling/sanger_workflow/whitelists/sanger/.*?"'
-RAW_DIR = "http://raw.github.com/ICGC-TCGA-PanCancer/pcawg-operations/master/variant_calling/sanger_workflow/whitelists/sanger/"
+
+RAW_DIR = "https://github.com/ICGC-TCGA-PanCancer/pcawg-operations/raw/develop/variant_calling/sanger_workflow/whitelists/sanger/"
 
 proxies = {"http" : "http://wwwcache.sanger.ac.uk:3128",
            "https": "http://wwwcache.sanger.ac.uk:3128"}
 
+class ProxyAdapter(HTTPAdapter):
+    def proxy_headers(self, proxy):
+        return {"User-agent": "Mozilla/5.0"}
+
+session = requests.Session()
+session.mount("https://", ProxyAdapter())
+
 class AnalysisCheck(object):
     def __init__(self):
         self.donors = self.load_donors()
-        #json_data = self.find_json_gz()
-        #self.load_json_data(json_data)
+        json_data = self.find_json_gz()
+        self.load_json_data(json_data)
     
     def find_json_gz(self):
         directory = self.open_url(DEFAULT_PATH)
-        filename = re.search("donor_p_\d+\.jsonl\.gz", directory.read()).group()
-        directory.close()
-        gz_file = StringIO.StringIO(opener.open(posixpath.join(DEFAULT_PATH, filename)).read())
+        filename = re.search("donor_p_\d+\.jsonl\.gz", directory).group()
+        gz_file = StringIO.StringIO(self.open_url(posixpath.join(DEFAULT_PATH, filename)))
         json_data = map(json.loads, gzip.GzipFile(fileobj=gz_file).readlines())
         return json_data
 
@@ -45,22 +58,25 @@ class AnalysisCheck(object):
 
     def load_donors(self):
         repo_dir = self.open_url(posixpath.join(GITHUB_URL, REPO_URL))
-        for file_match in re.findall(SEARCH_FILES, repo_dir.read()):
+        donors = []
+        for file_match in re.findall(SEARCH_FILES, repo_dir):
             filename = file_match.split(posixpath.sep)[-1][:-1]
-            self.load_donor_file(posixpath.join(RAW_DIR, filename))
-            print "Returned"
+            donors.extend(self.load_donor_file(posixpath.join(RAW_DIR, filename)))
         #donors = map(lambda line: line[:-1].replace("\t", "::"), donor_file.readlines())
-        #return donors
+        return donors
 
     def load_donor_file(self, donor_name):
+        donors = []
         donor_file = self.open_url(donor_name)
-        print donor_file.read()
-        donor_file.close()
+        for line in donor_file.split("\n"):
+            if line != "":
+                donors.append(line.replace("\t", "::"))
+        return donors
 
     def open_url(self, url):
-        r = requests.get(url, proxies=proxies)
-        print r.content
-        return r.raw
+        r = session.get(url, proxies=proxies)
+        return r.content
+
 
 def main():
     AnalysisCheck()

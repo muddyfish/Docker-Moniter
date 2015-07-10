78b9f1ce08e6a73185d8e8fd1176ba19a1d8f1f9
Simon Beal <sb48@sanger.ac.uk>
2015-07-10 15:19:44 +0100

    Fix Proxy Test Made more obvious its for python 2.7.9

--- test_proxy.py
+++ test_proxy.py
@@ -1,7 +1,15 @@
+#Python-2.7.9
 import requests
+from requests.adapters import HTTPAdapter
 
 proxies = {"http" : "http://wwwcache.sanger.ac.uk:3128",
            "https": "http://wwwcache.sanger.ac.uk:3128"}
 
-r = requests.get("https://raw.githubusercontent.com/ICGC-TCGA-PanCancer/pcawg-operations/develop/variant_calling/sanger_workflow/whitelists/sanger/sanger.150501-1028.from_cghub.txt", proxies=proxies)
+class ProxyAdapter(HTTPAdapter):
+    def proxy_headers(self, proxy):
+        return {"User-agent": "Mozilla/5.0"}
+
+session = requests.Session()
+session.mount("https://", ProxyAdapter())
+r = session.get("https://raw.githubusercontent.com/ICGC-TCGA-PanCancer/pcawg-operations/develop/variant_calling/sanger_workflow/whitelists/sanger/sanger.150501-1028.from_cghub.txt", proxies=proxies)
 print(r.content)
